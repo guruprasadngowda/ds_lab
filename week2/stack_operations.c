67269671b3dd6b4148d75c96df50ce26b5aeee35
guruprasadngowda <guruprasadn.cs24@bmsce.ac.in>
2025-10-18 10:56:11 +0530

    Update stack_operations.c

--- week2/stack_operations.c
+++ week2/stack_operations.c
@@ -1,1 +1,61 @@
+#include <stdio.h>
+#define MAX 2
 
+int stack[MAX];
+int top = -1;
+
+void push(int value) {
+    if (top == MAX - 1) {
+        printf("Stack Overflow: Cannot push %d, stack is full.\n", value);
+    } else {
+        stack[++top] = value;
+        printf("Pushed %d onto the stack.\n", value);
+    }
+}
+
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow: Cannot pop, stack is empty.\n");
+    } else {
+        printf("Popped %d from the stack.\n", stack[top--]);
+    }
+}
+
+void display() {
+    if (top == -1) {
+        printf("Stack is empty.\n");
+    } else {
+        printf("Stack contents (top to bottom):\n");
+        for (int i = top; i >= 0; i--) {
+            printf("%d\n", stack[i]);
+        }
+    }
+}
+
+int main() {
+    int choice, value;
+    while (1) {
+        printf("\nChoose an operation:\n");
+        printf("1. Push\n2. Pop\n3. Display\n4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                printf("Enter value to push: ");
+                scanf("%d", &value);
+                push(value);
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                printf("Exiting program.\n");
+                return 0;
+            default:
+                printf("Invalid choice. Try again.\n");
+        }
+    }
+}
